27c280ffb649083f8bfe4262b8022ed1d5b373ca
Fiza <131092372+Fizahere@users.noreply.github.com>
2024-09-19 13:23:07 +0500

    [gallery]

---
+++ src/ClientSite/Gallery.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { SimpleGrid, Text, Box } from '@chakra-ui/react';
+import GalleryCard from '../components/Mist/GalleryCard';
+
+function Gallery() {
+    const designs = [
+        {
+            name: 'Elegant Kitchen Design',
+            image: 'https://www.janelockhart.com/portfolio/images/cottage-core-chic-04.jpg',
+            height: '250px',
+        },
+        {
+            name: 'Cozy Living Room Design',
+            image: 'https://www.decorilla.com/online-decorating/wp-content/uploads/2022/11/Luxury-interior-design-home-designing-1-scaled.jpeg',
+        },
+        {
+            name: 'Elegant Kitchen Design',
+            image: 'https://www.janelockhart.com/portfolio/images/cottage-core-chic-04.jpg',
+        },
+        {
+            name: 'Cozy Living Room Design',
+            image: 'https://www.decorilla.com/online-decorating/wp-content/uploads/2022/11/Luxury-interior-design-home-designing-1-scaled.jpeg',
+        },
+        {
+            name: 'Elegant Kitchen Design',
+            image: 'https://miro.medium.com/v2/resize:fit:1400/1*RQ6RPO0mUf7QYuOkyvXARA.jpeg',
+        },
+        {
+            name: 'Cozy Living Room Design',
+            image: 'https://www.decorilla.com/online-decorating/wp-content/uploads/2022/11/Luxury-interior-design-home-designing-1-scaled.jpeg',
+        },
+        {
+            name: 'Elegant Kitchen Design',
+            image: 'https://i.pinimg.com/736x/eb/0c/23/eb0c23bf3f195214da6998236bfb3ed8.jpg',
+        },
+        {
+            name: 'Cozy Living Room Design',
+            image: 'https://www.decorilla.com/online-decorating/wp-content/uploads/2022/11/Luxury-interior-design-home-designing-1-scaled.jpeg',
+        },
+    ];
+
+    return (
+        <>
+            <Text fontSize={'2rem'} fontWeight={'bold'} borderBottom={'1px solid grey'}>
+                Gallery
+            </Text>
+            <Box py={8} className="collection">
+                <SimpleGrid
+                    columns={{ base: 1, sm: 2, md: 3, lg: 4 }}
+                    spacing={6}
+                >
+                    {designs.map((singleDesign, index) => (
+                        <GalleryCard key={index} cardData={singleDesign} />
+                    ))}
+                </SimpleGrid>
+            </Box>
+        </>
+    );
+}
+
+export default Gallery;
--- src/ClientSite/Services.jsx
+++ src/ClientSite/Services.jsx
@@ -1,102 +1,99 @@
-// import React from "react";
-// import { Box, Flex, Icon, SimpleGrid, Text } from "@chakra-ui/react";
-// import { TypeAnimation } from "react-type-animation";
-// import APP_ICONS from "../constants/icons";
-// import { Colors } from "../constants/colors";
+import React from "react";
+import { Box, Flex, Icon, SimpleGrid, Text } from "@chakra-ui/react";
+import { TypeAnimation } from "react-type-animation";
+import APP_ICONS from "../constants/icons";
+import { Colors } from "../constants/colors";
 
-// function Services() {
-//   return (
-//     <>
-//       <Box>
-//         <Flex
-//           flexDirection={{ base: "column", md: "row" }}
-//           justifyContent={"space-between"}
-//         >
-//           <Box width={"40%"}>
-//             <TypeAnimation
-//               sequence={[
-//                 "WHAT KIND OF SERVICES WE PROVIDE TO OUR CLIENTS",
-//                 1000,
-//               ]}
-//               wrapper="span"
-//               speed={50}
-//               style={{
-//                 fontSize: "3rem",
-//                 fontWeight: "bold",
-//                 display: "inline-block",
-//               }}
-//               repeat={1}
-//             />
-//           </Box>
-//           <Box width={"40%"}>
-//             <SimpleGrid p={"10px"} columns={{ base: 1, sm: 2 }} spacing={4}>
-//               <Box
-//                 display={"flex"}
-//                 flexDirection={"column"}
-//                 alignItems={"center"}
-//                 justifyContent={"center"}
-//                 bg={Colors.WHITE}
-//                 height={40}
-//                 width={170}
-//                 p={2}
-//                 borderRadius={20}
-//               >
-//                 <Icon fontSize={30} as={APP_ICONS.PENCILSCALE} fontWeight={"bold"} />
-//                 <Text mt={5} textAlign={"center"}>CUSTOM HOME DESIGN</Text>
-//               </Box>
-//               <Box
-//                 display={"flex"}
-//                 flexDirection={"column"}
-//                 alignItems={"center"}
-//                 justifyContent={"center"}
-//                 bg={Colors.WHITE}
-//                 height={'200px'}
-//                 width={210}
-//                 p={5}
-//                 borderRadius={20}
-//               >
-//                 <Icon fontSize={30} as={APP_ICONS.HAMMER} />
-//                 <Text  mt={5} textAlign={"center"}>INTERIOR AND EXTERIOR DESIGN</Text>
-//               </Box>
-//               <Box
-//                 display={"flex"}
-//                 flexDirection={"column"}
-//                 alignItems={"center"}
-//                 justifyContent={"center"}
-//                 bg={Colors.WHITE}
-//                 height={'200px'}
-//                 width={210}
-//                 p={5}
-//                 borderRadius={20}
-//               >
-//                 <Icon fontSize={30} as={APP_ICONS.COMPASS} />
-//                 <Text mt={5} textAlign={"center"}>RENOVATION AND RESTORATION</Text>
-//               </Box>
+function Services() {
+  return (
+    <>
+      <Box mt={10}>
+        <Flex
+          flexDirection={{ base: "column", md: "row" }}
+          justifyContent={"space-between"}
+        >
+          <Box width={"40%"}>
+            <TypeAnimation
+              sequence={[
+                "WHAT KIND OF SERVICES WE PROVIDE TO OUR CLIENTS",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              style={{
+                fontSize: "3rem",
+                fontWeight: "bold",
+                display: "inline-block",
+              }}
+              repeat={1}
+            />
+          </Box>
+          <Box width={"40%"}>
+            <SimpleGrid p={"10px"} columns={{ base: 1, sm: 2 }} spacing={4}>
+              <Box
+                display={"flex"}
+                flexDirection={"column"}
+                alignItems={"center"}
+                justifyContent={"center"}
+                bg={Colors.WHITE}
+                height={40}
+                width={170}
+                p={2}
+                borderRadius={20}
+              >
+                <Icon fontSize={30} as={APP_ICONS.PENCILSCALE} fontWeight={"bold"} />
+                <Text mt={5} textAlign={"center"}>CUSTOM HOME DESIGN</Text>
+              </Box>
+              <Box
+                display={"flex"}
+                flexDirection={"column"}
+                alignItems={"center"}
+                justifyContent={"center"}
+                bg={Colors.WHITE}
+                height={'200px'}
+                width={210}
+                p={5}
+                borderRadius={20}
+              >
+                <Icon fontSize={30} as={APP_ICONS.HAMMER} />
+                <Text  mt={5} textAlign={"center"}>INTERIOR AND EXTERIOR DESIGN</Text>
+              </Box>
+              <Box
+                display={"flex"}
+                flexDirection={"column"}
+                alignItems={"center"}
+                justifyContent={"center"}
+                bg={Colors.WHITE}
+                height={'200px'}
+                width={210}
+                p={5}
+                borderRadius={20}
+              >
+                <Icon fontSize={30} as={APP_ICONS.COMPASS} />
+                <Text mt={5} textAlign={"center"}>RENOVATION AND RESTORATION</Text>
+              </Box>
 
-//               <Box
-//                 display={"flex"}
-//                 flexDirection={"column"}
-//                 alignItems={"center"}
-//                 justifyContent={"center"}
-//                 bg={Colors.WHITE}
-//                 height={40}
-//                 width={170}
-//                 p={5}
-//                 borderRadius={20}
-//               >
-//                 <Icon fontSize={30} as={APP_ICONS.BUILDINGS} />
-//                 <Text  mt={5} textAlign={"center"}>COMMERCIAL ARCHITECHTURE</Text>
-//               </Box>
-//             </SimpleGrid>
-//           </Box>
-//         </Flex>
-//       </Box>
-//     </>
-//   );
-// }
-
-// export default Services;
-const Services=()=>{
-  return(<></>)
+              <Box
+                display={"flex"}
+                flexDirection={"column"}
+                alignItems={"center"}
+                justifyContent={"center"}
+                bg={Colors.WHITE}
+                height={40}
+                width={170}
+                p={5}
+                borderRadius={20}
+              >
+                <Icon fontSize={30} as={APP_ICONS.BUILDINGS} />
+                <Text  mt={5} textAlign={"center"}>COMMERCIAL ARCHITECHTURE</Text>
+              </Box>
+            </SimpleGrid>
+          </Box>
+        </Flex>
+      </Box>
+    </>
+  );
 }
-export default Services
+
+export default Services;
+
