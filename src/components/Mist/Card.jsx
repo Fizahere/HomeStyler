521144b90f51e9619189761772136b72a622dfe7
Nasreen <nasreenmalik429@gmail.com>
2024-09-19 17:25:07 +0500

    tabs onChange

--- src/components/Mist/Card.jsx
+++ src/components/Mist/Card.jsx
@@ -13,7 +13,6 @@ import {
 import { useNavigate } from "react-router-dom";
 import { Colors } from "../../constants/colors";
 import { UnAuthenticatedRoutesNames } from "../../utilities/util.constant";
-// import { imageMap } from "../../assets/constants/images";
 
 function CustomCard({ singleProduct }) {
   // console.log(singleProduct, 'singleProduct');
