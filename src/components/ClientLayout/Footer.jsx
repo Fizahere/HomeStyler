dcd32850643be6c19c8e61533bad134cac942d9d
Fiza <131092372+Fizahere@users.noreply.github.com>
2024-09-20 10:11:02 +0500

    [sitemap]

--- src/components/ClientLayout/Footer.jsx
+++ src/components/ClientLayout/Footer.jsx
@@ -8,7 +8,7 @@ import { UnAuthenticatedRoutesNames } from "../../utilities/util.constant";
 function Footer() {
   return (
     <>
-      <Box h={"auto"} borderTop={"1px solid grey"} >
+      <Box p={2} h={"auto"} borderTop={"1px solid grey"} >
         <DomLink to={"/"}>
           <Flex mt={4}>
             {/* <Icon as={APP_ICONS.} fontSize={{ base: 20, md: 25 }} />{" "} */}
@@ -83,9 +83,6 @@ function Footer() {
           </DomLink>
           <DomLink to={UnAuthenticatedRoutesNames.CONTACT}>
             <Text>Contact</Text>
-          </DomLink>
-          <DomLink to={UnAuthenticatedRoutesNames.SHOP}>
-            <Text>Shop</Text>
           </DomLink>
           <DomLink to={UnAuthenticatedRoutesNames.FEEDBACK}>
             <Text>Feedback</Text>
