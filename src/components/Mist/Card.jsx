79af64c37bd0535c7cd2b9e06fa9eeb5d4d2899f
Nasreen <nasreenmalik429@gmail.com>
2024-09-20 17:30:56 +0500

    products images

--- src/components/Mist/Card.jsx
+++ src/components/Mist/Card.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Card,
   Image,
@@ -15,10 +15,26 @@ import {
 import { useNavigate } from "react-router-dom";
 import { Colors } from "../../constants/colors";
 import { UnAuthenticatedRoutesNames } from "../../utilities/util.constant";
-import { designImagesMap } from "../../constants/images";
+import { designImagesMap, productsImagesMap } from "../../constants/images";
 
 function CustomCard({ singleProduct, isLoading }) {
-  // console.log(singleProduct?.isProduct,'singleProduct');
+  const [isDesignMode, setIsDesignMode] = useState(false);
+  const [isProductMode, setIsProductMode] = useState(false);
+  // console.log(singleProduct.image,'singleProduct');
+  console.log(productsImagesMap.PROD1, "productsImagesMap");
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (singleProduct?.isProduct) {
+      setIsProductMode(true);
+      setIsDesignMode(false);
+    } else if (singleProduct?.isDesign) {
+      setIsDesignMode(true);
+      setIsProductMode(false);
+    }
+  }, [singleProduct]);
+
   const handleButtonClick = () => {
     if (singleProduct?.isProduct) {
       navigate(
@@ -35,6 +51,19 @@ function CustomCard({ singleProduct, isLoading }) {
         )}`
       );
     }
+  };
+  const { id, name, description, price, category, image } = singleProduct;
+  // console.log(image,'image')
+  const imageUrl = designImagesMap[image];
+  // console.log(imageUrl,'image')
+  const ProductsList = () => {
+    return (
+      <div>
+        {Object.entries(productsImagesMap).map(([key, src]) => (
+          <img key={key} src={src} alt={key} />
+        ))}
+      </div>
+    );
   };
 
   if (isLoading) {
@@ -54,41 +83,52 @@ function CustomCard({ singleProduct, isLoading }) {
     );
   }
 
-  const { id, name, description, price, category, image } = singleProduct;
-  const imageUrl = designImagesMap[image];
-
-  const navigate = useNavigate();
-
   return (
+    <>
+    {Object.entries(productsImagesMap).map(([key, src]) => (
     <Card
-      boxShadow={
-        "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
-      }
-      _dark={{ bg: "transparent" }}
-      maxW={{ base: "xs", md: "xs" }}
+    boxShadow={
+      "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
+    }
+    _dark={{ bg: "transparent" }}
+    maxW={{ base: "xs", md: "xs" }}
     >
       <CardBody>
-        <Image
+        {isDesignMode ? (
+          <Image
           src={imageUrl}
-          alt={"image_thumbnail_path"}
-          borderRadius="md"
-          width={"100%"}
-          height={{ base: "250px", md: "250px" }}
-        />
+            alt={"image_thumbnail_path"}
+            borderRadius="md"
+            width={"100%"}
+            height={{ base: "250px", md: "250px" }}
+            />
+          ):(
+            <Image
+            src={src}
+              alt={"image_thumbnail_path"}
+              borderRadius="md"
+              width={"100%"}
+              height={{ base: "250px", md: "250px" }}
+              />
+        )}
         <Divider
           orientation="horizontal"
           borderColor="inherit"
           borderWidth="1px"
           mr={3}
-        />
+          />
         <Stack mt="6" spacing={{ base: "1", md: "3" }}>
           <Flex justifyContent={"space-between"} h={35}>
             <Heading size={{ base: "sm", md: "md" }}>{name}</Heading>
           </Flex>
-            <Text mt={1} color="green" fontWeight={'bold'} fontSize={{ base: "12px", md: "1xl" }}>
-              $ {price}
-            </Text>
-          {/* <Text fontSize={{ base: "12px", md: "1xl" }}>{category}</Text> */}
+          <Text
+            mt={1}
+            color="green"
+            fontWeight={"bold"}
+            fontSize={{ base: "12px", md: "1xl" }}
+            >
+            $ {price}
+          </Text>
 
           <Button
             mt={{ base: "1", md: "0" }}
@@ -99,17 +139,16 @@ function CustomCard({ singleProduct, isLoading }) {
               color: "",
             }}
             fontSize={{ base: "14px", md: "1xl" }}
-            onClick={() => {
-              handleButtonClick(id);
-            }}
-          >
+            onClick={handleButtonClick} // Corrected the handleButtonClick call
+            >
             Show more
           </Button>
         </Stack>
       </CardBody>
     </Card>
+          ))}
+    </>
   );
 }
-// handleButtonClick(id);
 
 export default CustomCard;
